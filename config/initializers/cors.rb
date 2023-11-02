83477b3dd3011d8e25d61ea80273fe5850b151b8
itsannaw <emikot321@gmail.com>
2023-11-03 00:35:31 +0300

    Deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,4 +5,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:5173'
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
+
+  allow do
+    origins "https://project-collectify.vercel.app"
+    resource "*",
+      headers: :any,
+      methods: :any, #[:get, :post, :put, :patch, :delete, :options, :head]
+      expose: [:Authorization]
+  end
 end
